a984c0cf2e0f80ba014515770182b2b653d7245d
zafer.torun <zafer.torun@cgi.com>
2020-01-10 12:39:31 +0100

    first commit

---
+++ src/test/java/com/zaf3r/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaf3r.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
