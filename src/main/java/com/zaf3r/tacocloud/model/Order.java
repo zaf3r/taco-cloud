ebccd35afa854de40183859f5e845784770e2932
zafer.torun <zafer.torun@cgi.com>
2020-01-14 22:13:48 +0100

    Added repositories and changed model classes to work with JPA

--- src/main/java/com/zaf3r/tacocloud/model/Order.java
+++ src/main/java/com/zaf3r/tacocloud/model/Order.java
@@ -2,8 +2,24 @@ package com.zaf3r.tacocloud.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private Date placedAt;
+
     private String name;
     private String street;
     private String city;
@@ -12,4 +28,16 @@ public class Order {
     private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void createdAt(){
+        this.placedAt = new Date();
+    }
 }
