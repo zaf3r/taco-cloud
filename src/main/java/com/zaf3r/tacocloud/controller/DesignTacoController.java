5764367809de2d4786e6293597d9588f12bec1ea
zafer.torun <zafer.torun@cgi.com>
2020-01-17 12:33:18 +0100

    refractored OrderController with to having rest endpoints instead of return pages, also made minor changes to DesignTacoController

--- src/main/java/com/zaf3r/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/zaf3r/tacocloud/controller/DesignTacoController.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import com.zaf3r.tacocloud.model.Ingredient.Type;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -38,4 +41,19 @@ public class DesignTacoController {
         return tacoRepo.findAll(page).getContent();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") long id){
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        if(optTaco.isPresent()){
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
 }
